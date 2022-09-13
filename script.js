4902d94395ccffb17a8963c43ba4e051bf8eec23
jaylawms19 <70493251+jaylawms19@users.noreply.github.com>
2022-09-12 23:53:17 -0500

    PR3
    Delete list items from to-do list with X button.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
+}
+
+var unchecked = document.getElementsByClassName("unchecked");
+var close = document.getElementsByClassName("close");
+
+// function checkElement(){
+//   var checkValue = document.getElementsByClassName("unchecked").value;
+//   alert(checkValue);
+// }
+
+// Removes list items when you click X
+function removeElement() {
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
 
 // Clicking on the "Add" button adds a new list item
@@ -20,4 +38,17 @@ function newElement() {
   }
   document.getElementById("todo").value = "";
 
+// show close button and deletes a list item when clicked
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
