0ab854b572ccd324a1682de1a303b525597e878b
jaylawms19 <70493251+jaylawms19@users.noreply.github.com>
2023-01-30 23:53:23 -0600

    PR2

--- notification.js
+++ notification.js
@@ -1,5 +1,31 @@
 notifyMe();
 
+
+spawnNotification();
+
+// 'use strict';
+
+// self.addEventListener('push', function(event) {
+//   console.log('Push Received.');
+//   console.log(`Push had this data: "${event.data.text()}"`);
+
+//   const title = 'Notifications Work';
+//   const options = {
+//     body: 'Awesome.'
+//   };
+
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
+
+new Notification(title, options)
+
+function spawnNotification(body, title) {
+    const notification = new Notification(title, { body });
+    title = "Hello, Jayla";
+    body = "Welcome to Portable Planner";
+    console.log("ran spawn");
+  }
+
 function notifyMe() {
     if (!("Notification" in window)) {
         // Check if the browser supports notifications
