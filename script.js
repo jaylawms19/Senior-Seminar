b19f57e7afb8468e319271714fa42423b243b6ab
jaylawms19 <70493251+jaylawms19@users.noreply.github.com>
2023-04-18 11:18:54 -0500

    Linking welcome page and main page

--- script.js
+++ script.js
@@ -116,6 +116,93 @@ function clearAll() {
   document.getElementById("list").innerHTML = "";
 }
 
+
+
+
+
+// Make the Schedule, To-Do, and Notes columns resizable by the user
+ 
+var sect = document.getElementsByClassName('col'); // element to make resizable
+console.log("this is sect: ", sect);
+
+var numSect = sect.length;
+
+function showP(e) {
+  for (var i = 0; i < numSect; i++) {
+    sect[i].className = sect[i].className + '-resizable';
+    var resizer = document.createElement('div');
+    resizer.className = 'resizer';
+    sect[i].appendChild(resizer);
+    resizer.addEventListener('mousedown', initDrag, true);
+  }
+}
+
+///
+
+  // sect.className = sect.className + '-resizable';
+  // var resizer = document.createElement('div');
+  // resizer.className = 'resizer';
+  // sect.appendChild(resizer);
+
+//   for (var i = 0; i < numSect; i++) {
+//     sect[i].removeEventListener('click', showP, false);
+//   }
+//   resizer.addEventListener('mousedown', initDrag, false);
+// }
+
+// for (var i = 0; i < numSect; i++) {
+//   sect[i].addEventListener('click', showP, false);
+// }
+
+
+
+//2nd
+// for (var i = 0 ; i < sect.length; i++) {
+//   sect[i].addEventListener('click' , init , false ) ; 
+//   sect[i].removeEventListener('click', init, false);
+//   sect[i].className = sect.className + ' resizable';
+//   var resizer = document.createElement('div');
+//   resizer.className = 'resizer';
+//   sect[i].appendChild(resizer);
+//   resizer.addEventListener('mousedown', initDrag, false);
+// }
+
+//1st
+// sect.addEventListener('click', function init() {
+//     sect.removeEventListener('click', init, false);
+//     sect.className = sect.className + ' resizable';
+//     var resizer = document.createElement('div');
+//     resizer.className = 'resizer';
+//     sect.appendChild(resizer);
+//     resizer.addEventListener('mousedown', initDrag, false);
+// }, false);
+
+var startX;
+// var startY; 
+var startWidth; 
+// var startHeight;
+
+function initDrag(e) {
+   startX = e.clientX;
+  //  startY = e.clientY;
+   startWidth = parseInt(document.defaultView.getComputedStyle(sect[0]).width, 10);
+  //  startHeight = parseInt(document.defaultView.getComputedStyle(sect).height, 10);
+   document.documentElement.addEventListener('mousemove', doDrag, true);
+   document.documentElement.addEventListener('mouseup', stopDrag, true);
+}
+
+function doDrag(e) {
+   sect[0].style.width = (startWidth + e.clientX - startX) + 'px';
+  //  sect.style.height = (startHeight + e.clientY - startY) + 'px';
+}
+
+function stopDrag(e) {
+    document.documentElement.removeEventListener('mousemove', doDrag, false);
+    document.documentElement.removeEventListener('mouseup', stopDrag, false);
+}
+
+
+
 // saving todo items in local storage
 
 //const taskInput = document.querySelector(".additem input");
@@ -159,3 +246,24 @@ function clearAll() {
   // taskBox.innerHTML = li;
 // }
 // showTodo();
+
+// function officeTheme() {
+//   var button2 = document.getElementsByClassName("button2");
+//   button2.addEventListener("click", changeClassb2);
+//   }
+
+// function changeClassb2() {
+//   document.className.replace("false","true");
+// }
+
+// const setTheme = theme => document.documentElement.className = theme;
+//     document.getElementById('theme-select').addEventListener('change', function() {
+//     setTheme(this.value);
+//     });
+
+//     @media (prefers-color-scheme: dark) {
+//       :root {
+//       --bg-color: #121212;
+//       --text-color: #45ADFE;
+//       }
+//       }
